e3f4f2277b4eac745c33761ec10cca318e00c0a6
Tu Van Nguyen <tuvannguyen33@gmail.com>
2026-02-16 19:28:47 -0600

    fix cyclometric complexity notes

--- test.py
+++ test.py
@@ -1,28 +1,16 @@
-from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+def removeDuplicates(s: str) -> str:
+    string_stack = []
 
-class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if not head or not head.next or left == right:
-            return head
-        if left == 1:
-            tail = head.next
-            tail_distance = right-2
-            while tail.next and tail_distance > 0:
-                tail = tail.next
-                tail_distance -= 1
-            after_tail = tail.next
-            tail.next = None
-            nextNode = self.reverseBetween(head.next, left, right - 1)
-            tail = nextNode
-            while tail.next:
-                tail = tail.next
-            tail.next = head
-            head.next = after_tail
-            head = nextNode
-        else:
-            head.next = self.reverseBetween(head.next, left-1, right-1)
-        return head
+    for i in range(len(4)):
+        for letter in s:
+            string_stack.append(letter)
+            if len(string_stack) >= 1 and string_stack[-1] == letter :
+                string_stack.pop()
+            elif string_stack[0] == letter:
+                print(letter)
+            elif string_stack[-1] == 'a':
+                print(letter)
+            else:
+                string_stack.append(letter)
+    return "".join(string_stack) # From Leetcode Problem from datetime import date
+
