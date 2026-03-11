40b79ef951ebf52ad505e8889c13bd949e4a48ad
Tu Van Nguyen <tuvannguyen33@gmail.com>
2026-03-10 20:47:35 -0500

    update monotonic queues and stacks problems

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-def removeDuplicates(s: str) -> str:
-    string_stack = []
+from typing import List
 
-    for i in range(len(4)):
-        for letter in s:
-            string_stack.append(letter)
-            if len(string_stack) >= 1 and string_stack[-1] == letter :
-                string_stack.pop()
-            elif string_stack[0] == letter:
-                print(letter)
-            elif string_stack[-1] == 'a':
-                print(letter)
-            else:
-                string_stack.append(letter)
-    return "".join(string_stack) # From Leetcode Problem from datetime import date
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+    answer = [0] * len(temperatures)
+    tracker  = []
+    for temp_index in range(len(temperatures)):
+        while tracker and temperatures[temp_index] > temperatures[tracker[-1]]:
+            last_index = tracker.pop()
+            answer[last_index] = temp_index - last_index
+        tracker.append(temp_index)
+    return answer
 
